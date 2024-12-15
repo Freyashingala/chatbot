96709b5a2f02171aeb48c7bb274eb08cf52eb7df
Freyashingala <152647083+Freyashingala@users.noreply.github.com>
2024-12-15 15:34:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 const app = express();
 app.use(cors({
-    origin: 'https://inter-iit-bootcamp-dev-task-frontend.vercel.app',
+    origin: '',
     methods: ['GET', 'POST'],
     credentials: true,
 }));
